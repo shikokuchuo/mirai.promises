2f4e483e5a8ab8b5a5a412ec1a5237e315a77b05
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2023-05-30 12:48:47 +0100

    update tests

--- tests/tests.R
+++ tests/tests.R
@@ -1,7 +1,5 @@
 library(mirai.promises)
 library(promises)
 nanotest <- function(x) invisible(x || stop("is not TRUE when expected to be TRUE"))
-
 nanotest(promises::is.promise(p1 <- promises::as.promise(mirai::mirai("completed"))))
-nanotest(promises::is.promise(p2 <- promises::as.promise(mirai::mirai(mirai::mirai()))))
 nanotest(promises::is.promise(p3 <- mirai::mirai("completed") %...>% identity()))
