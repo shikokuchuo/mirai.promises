c52208c18b71403c5e3825fe2365283b5b298f02
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2023-07-31 10:29:00 +0100

    efficiency

--- R/core.R
+++ R/core.R
@@ -53,7 +53,7 @@ as.promise.mirai <- function(x) {
       query <- function()
         if (unresolved(x))
           later(query, delay = 0.1) else
-            if (is_error_value(value <- .subset2(x, "data")))
+            if (is_error_value(value <- parent.env(x)[["result"]]))
               reject(value) else
                 resolve(value)
       query()
